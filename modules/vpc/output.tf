75e5f2aa1c8f178d80de4db21eec47d06c1583c8
archana0424 <sarchana0517@gmail.com>
2026-03-14 09:21:34 +0000

    changes in vpc output

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,2 +1,2 @@
 output "vpc_id" { value = google_compute_network.vpc.id}
-output "vpc_self_link" { value = google_compute_network.vpc.vpc_self_link}
+output "vpc_self_link" { value = google_compute_network.vpc.self_link}
