e35b2f801e48029939e7551c137f04da367e80b0
archana0424 <sarchana0517@gmail.com>
2026-03-14 13:05:11 +0000

    added module peering

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ module "vpcs" {
 
 module "vpc_peering" {
   source = "./modules/vpc_peering"
-  vpc_a  = module.vpcs[var.vpc_peering.vpc_a].self_link
-  vpc_b  = module.vpcs[var.vpc_peering.vpc_b].self_link
+  vpc_a  = module.vpcs[var.vpc_peering.vpc_a].vpc_self_link
+  vpc_b  = module.vpcs[var.vpc_peering.vpc_b].vpc_self_link
 }
